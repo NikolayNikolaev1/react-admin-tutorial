cec7f4e0101066d77f8fe1a3bb26b033ba21e229
HP-ELITEBOOK\Developer <n.nikolaevbg97@gmail.com>
2022-06-06 19:44:48 +0300

    Handling relationships.

--- test-admin/src/App.js
+++ test-admin/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { Admin } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import UserList from './users';
+import PostList from './posts';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
-const App = () => <Admin dataProvider={dataProvider}/>
+const App = () => (
+    <Admin dataProvider={dataProvider} >
+        <Resource name="posts" list={PostList} />
+        <Resource name="users" list={UserList} />
+    </Admin>
+);
 
 export default App;
