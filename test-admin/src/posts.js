355fa038a887cac23a6adc0c150b1b6f70767a4e
HP-ELITEBOOK\Developer <n.nikolaevbg97@gmail.com>
2022-06-07 12:55:50 +0300

    Added editing and creation capabilities, customized the page title and added search and filter to the list.

--- test-admin/src/posts.js
+++ test-admin/src/posts.js
@@ -1,15 +1,78 @@
 import React from 'react';
-import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
+import { useMediaQuery } from '@mui/material';
+import {
+    Create,
+    Datagrid,
+    Edit,
+    EditButton,
+    List,
+    ReferenceField,
+    ReferenceInput,
+    SelectInput,
+    SimpleForm,
+    SimpleList,
+    TextField,
+    TextInput,
+    useRecordContext
+} from 'react-admin';
 
-export default () => (
-    <List>
-        <Datagrid>
-            <TextField source="id" />
-            <ReferenceField source="userId" reference="users">
-                <TextField source="name" />
-            </ReferenceField>
-            <TextField source="title" />
-            <EditButton />
-        </Datagrid>
-    </List>
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Create>
 );
+
+export const PostEdit = () => (
+    <Edit title={<PostTitle />}>
+        <SimpleForm>
+            <TextInput disabled source="id" />
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+const postFilters = [
+    <TextInput source="q" label="Search" alwaysOn />,
+    <ReferenceInput source="userId" label="User" reference="users">
+        <SelectInput optionText="name" />
+    </ReferenceInput>
+];
+
+export const PostList = () => {
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+    return (
+        <List filters={postFilters}>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={record => record.title}
+                    secondaryText={record => `${record.views} views`}
+                    tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
+                />
+            ) : (
+                <Datagrid>
+                    <TextField source="id" />
+                    <ReferenceField source="userId" reference="users">
+                        <TextField source="name" />
+                    </ReferenceField>
+                    <TextField source="title" />
+                    <EditButton />
+                </Datagrid>
+            )}
+        </List>
+    );
+}
+
+const PostTitle = () => {
+    const record = useRecordContext();
+    return (<span>Post {record ? `${record.title}` : ''}</span>);
+}
