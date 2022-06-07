b26bec5eb2068a0bd91cfe8dfa7af232db3cc066
HP-ELITEBOOK\Developer <n.nikolaevbg97@gmail.com>
2022-06-07 12:58:14 +0300

    Customized the menu icons.

--- test-admin/src/App.js
+++ test-admin/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import authProvider from './authProvider';
+import Dashboard from './Dashboard';
+import PostIcon from '@mui/icons-material/Book';
+import UserIcon from '@mui/icons-material/Group';
 import UserList from './users';
-import PostList from './posts';
+import { PostCreate, PostEdit, PostList } from './posts';
+
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
-    <Admin dataProvider={dataProvider} >
-        <Resource name="posts" list={PostList} />
-        <Resource name="users" list={UserList} />
+    <Admin authProvider={authProvider} dashboard={Dashboard} dataProvider={dataProvider} >
+        <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} icon={PostIcon} />
+        <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
 );
 
