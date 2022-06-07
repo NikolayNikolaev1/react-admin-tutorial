2dc530b8418853f03edf3fec9c935a8c3ee0dca0
HP-ELITEBOOK\Developer <n.nikolaevbg97@gmail.com>
2022-06-07 12:56:22 +0300

    Using a custom home page.

---
+++ test-admin/src/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Card, CardContent, CardHeader } from '@mui/material';
+
+export default () => (
+    <Card>
+        <CardHeader title="Welcome to administration panel" />
+        <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+    </Card>
+);
